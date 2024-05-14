5826a3a11a9114d3325452f166612efb2e72cbde
Sunamra <dassunamra@gmail.com>
2024-05-14 23:21:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+document.onkeydown = function (e) {
+	if (e.keyCode == 123) { // F12 key
+		return false;
+	}
+};
 
+document.addEventListener('contextmenu', function (event) {
+	event.preventDefault();
+});
